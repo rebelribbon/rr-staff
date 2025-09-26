ef4244c701e0f4198985ac2be78fa42f47fa5537
rebelribbon <brad@rebelribbon.com>
2025-09-26 18:34:09 -0500

    Update signin.js

--- pages/signin.js
+++ pages/signin.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { getBrowserClient } from "@/lib/supabaseBrowser";
+import { getBrowserClient } from "../lib/supabaseBrowser";
+
 
 export default function SignIn() {
   const supabase = getBrowserClient();
