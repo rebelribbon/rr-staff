83a35e56abedbb0ae207186e9e33451c0a4a1674
rebelribbon <brad@rebelribbon.com>
2025-09-28 12:57:48 -0500

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+// next.config.mjs
+import path from "path";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias["@" ] = path.resolve(process.cwd());
+    return config;
+  },
+};
+
+export default nextConfig;
